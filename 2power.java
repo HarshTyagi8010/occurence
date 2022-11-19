b069c1311f05afbfd44abd3c198b8b71cdd53622
Harsh Tyagi <117800830+HarshTyagi8010@users.noreply.github.com>
2022-11-19 19:25:21 +0530

    Add files via upload

---
+++ 2power.java
@@ -0,0 +1,22 @@
+public class 2power {
+    public static boolean powerOf(int i) {
+        if(i<0){
+            return false;
+        }
+        if(i==){
+            return true;
+        }
+        if(i%2==0){
+            return power(i/2){
+                return false
+            }
+        }
+
+        
+    }
+    public static void main(String[] args) {
+        powerOf
+       
+    }
+    
+}
