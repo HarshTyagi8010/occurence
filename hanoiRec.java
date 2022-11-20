09a62ce63e27137a2c52b7d9df86bdeecbfff928
Harsh Tyagi <117800830+HarshTyagi8010@users.noreply.github.com>
2022-11-20 10:29:05 +0530

    Add files via upload

---
+++ hanoiRec.java
@@ -0,0 +1,22 @@
+public class hanoiRec {
+    public static void towerOfHanoi(int n,String src,String helper,String dest) {
+        if(n==1){
+            System.out.println("transfer disk"+ n +" from "+src+" to "+dest);
+            return;
+        
+        }
+        towerOfHanoi(n-1, src, dest, helper);
+        System.out.println("transfer disk"+ n +" from "+src+" to "+dest);
+        towerOfHanoi(n-1, helper, src, dest); 
+
+        
+    }
+    public static void main(String[] args) {
+        int =1;
+        towerOfHanoi(n, "S", "H", "D");
+    }
+
+        
+      
+    
+}
